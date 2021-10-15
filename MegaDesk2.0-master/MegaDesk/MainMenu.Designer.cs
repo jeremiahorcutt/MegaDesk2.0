d4d6468cebcf43b55177d3f60b87cce260caf34b
billburton405@gmail.com <billburton405@gmail.com>
2021-10-15 16:23:58 -0700

    1015/21

--- MegaDesk2.0-master/MegaDesk/MainMenu.Designer.cs
+++ MegaDesk2.0-master/MegaDesk/MainMenu.Designer.cs
@@ -164,7 +164,7 @@ namespace MegaDesk
             this.btnMedia.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
             this.btnMedia.Size = new System.Drawing.Size(251, 53);
             this.btnMedia.TabIndex = 0;
-            this.btnMedia.Text = "&Desk Ouote";
+            this.btnMedia.Text = "&Menu Options";
             this.btnMedia.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnMedia.UseVisualStyleBackColor = true;
             this.btnMedia.Click += new System.EventHandler(this.btnMedia_Click);
